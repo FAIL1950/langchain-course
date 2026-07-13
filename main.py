631808cc9efeea00b11ee486bba349969d9a67d0
FAIL1950 <144266349+FAIL1950@users.noreply.github.com>
2026-07-13 10:38:29 +0300

    tavily tool

--- main.py
+++ main.py
@@ -2,30 +2,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from langchain.agents import create_agent
-from langchain.tools import tool
-from langchain_core.messages import HumanMessage
-from langchain_google_genai import ChatGoogleGenerativeAI
-from tavily import TavilyClient
-
-tavily = TavilyClient()
-
-
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search result
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query=query)
-
+from langchain.agents import create_agent  # noqa: E402
+from langchain_core.messages import HumanMessage  # noqa: E402
+from langchain_google_genai import ChatGoogleGenerativeAI  # noqa: E402
+from langchain_tavily import TavilySearch  # noqa: E402
 
 llm = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite", temperature=0.0)
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 # llm_with_grounding = llm.bind(tools=[{"google_search": {}}])
 # agent = create_agent(model=llm_with_grounding)
@@ -34,7 +17,11 @@ agent = create_agent(model=llm, tools=tools)
 def main():
     print("Hello from langchain-course!")
     result = agent.invoke(
-        {"messages": [HumanMessage(content="What is the weather in Tokyo?")]}
+        {
+            "messages": HumanMessage(
+                content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details"
+            )
+        }
     )
     print(result)
 
